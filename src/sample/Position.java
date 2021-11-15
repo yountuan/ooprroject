19de950efafb7de4d66d5ba3e44275e9cb6962ea
Aida Osmonova <73512902+yountuan@users.noreply.github.com>
2021-11-16 01:05:10 +0600

    first commits

---
+++ src/sample/Position.java
@@ -0,0 +1,27 @@
+package sample;
+
+public class Position {
+    private final double xPos;
+    private final double yPos;
+
+    public Position(double xPos, double yPos) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+
+    public double getXPos() {
+        return xPos;
+    }
+
+    public double getYPos() {
+        return yPos;
+    }
+
+    @Override
+    public String toString() {
+        return "Position{" +
+                "xPos=" + xPos +
+                ", yPos=" + yPos +
+                '}';
+    }
+}
